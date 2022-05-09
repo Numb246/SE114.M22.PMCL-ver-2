0c2f5bc0e4dab1f1be270cb9fc0f6ae1072dc87a
Numb246 <77034191+Numb246@users.noreply.github.com>
2022-05-10 00:26:11 +0700

    1

--- FoodApp/app/src/main/java/com/vuquochung/foodapp/ui/cart/CartFragment.java
+++ FoodApp/app/src/main/java/com/vuquochung/foodapp/ui/cart/CartFragment.java
@@ -14,7 +14,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
-import android.view.animation.LayoutAnimationController;
+    import android.view.animation.LayoutAnimationController;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.RadioButton;
